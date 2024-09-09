ad1acecd5d967682cd498e5bc7a0ff5b705a0d69
Parameswari Bala <parameswaribala@gmail.com>
2024-09-09 12:17:18 +0530

    updated

--- advjavaws/src/main/java/com/siemens/utils/BuiltInFunctionalInterfaces.java
+++ advjavaws/src/main/java/com/siemens/utils/BuiltInFunctionalInterfaces.java
@@ -1,10 +1,12 @@
 package com.siemens.utils;
 
+import com.github.javafaker.Faker;
 import com.siemens.dao.ProductDAO;
 import com.siemens.dao.ProductImpl;
 import com.siemens.models.Product;
 
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -17,7 +19,7 @@ public class BuiltInFunctionalInterfaces {
             productDAO.addProduct(ComparatorLambdaDemo.createProduct());
         }
 
-        //filter products > $10 using Function
+        //filter products > $90 using Function
 
         Function<List<Product>,List<String>> filteredProductList=(productList)-> {
          return productList.stream().filter(p->p.getCost()>90.00)
@@ -30,6 +32,33 @@ public class BuiltInFunctionalInterfaces {
                 .stream()
                 .forEach(System.out::println);
 
+      //filter products starting with letter b
+      System.out.println("Starting with b ..................................");
+        filteredProductList=(productList)-> {
+            return productList.stream().filter(p->p.getName().startsWith("b"))
+                    .map(p->p.getName())
+                    .collect(Collectors.toList());
+        };
+
+        //invoke the Function
+        filteredProductList.apply(productDAO.getAllProducts())
+                .stream()
+                .forEach(System.out::println);
+
+        //input product and discount and output is final price
+        System.out.println("Print Final Price");
+        BiFunction<Product,Float,Double> finalPrice=(product,discount)->{
+           return product.getCost()- (product.getCost()*discount);
+
+        };
+        Faker faker=new Faker();
+       System.out.println(finalPrice.apply(new Product(faker.random().nextInt(10000),
+                faker.food().vegetable().toLowerCase(),
+                faker.random().nextInt(10000),
+                faker.food().measurement(),
+                Double.parseDouble(faker.commerce().price())),0.05f));
+       
+       //compare product1 unit with product 2 unit and say are they same or not
 
 
     }
