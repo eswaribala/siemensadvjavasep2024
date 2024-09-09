202c08710167b8fe8fd56d93bacb12b8744048cb
Parameswari Bala <parameswaribala@gmail.com>
2024-09-09 11:20:16 +0530

    updated

--- advjavaws/src/main/java/com/siemens/utils/ComparatorLambdaDemo.java
+++ advjavaws/src/main/java/com/siemens/utils/ComparatorLambdaDemo.java
@@ -5,6 +5,8 @@ import com.siemens.dao.ProductDAO;
 import com.siemens.dao.ProductImpl;
 import com.siemens.models.Product;
 
+import java.util.Comparator;
+
 public class ComparatorLambdaDemo {
 
     public static void main(String[] args){
@@ -15,8 +17,22 @@ public class ComparatorLambdaDemo {
             productDAO.addProduct(createProduct());
         }
 
+
+        //without stream how to do comparison
+        Comparator<Product> comparator=(p1,p2)->{
+            if(p1.getCost()>p2.getCost())
+                return 1;
+            else if (p1.getCost()<p2.getCost()) {
+                return -1;
+
+            }else
+                return 0;
+        };
+        productDAO.getAllProducts().sort(comparator);
         for(Product product:productDAO.getAllProducts())
             System.out.println(product);
+
+
 
     }
 
