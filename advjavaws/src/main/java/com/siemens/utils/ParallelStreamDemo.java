3a7bf938db9e61cd765329b402d4c600ec4267f2
Parameswari Bala <parameswaribala@gmail.com>
2024-09-09 15:22:31 +0530

    updated

--- advjavaws/src/main/java/com/siemens/utils/ParallelStreamDemo.java
+++ advjavaws/src/main/java/com/siemens/utils/ParallelStreamDemo.java
@@ -1,12 +1,23 @@
 package com.siemens.utils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 public class ParallelStreamDemo {
     public static void main(String[] args){
         File fileName=new File("Customer Purchasing Behaviors.csv");
-        if(fileName.exists())
+        if(fileName.exists()) {
             System.out.println("Exists");
+            try {
+               Stream<String> lines= Files.lines(fileName.toPath());
+               lines.parallel().forEach(System.out::println);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 
 }
