90ad1eadceb7a7404d6adbba8d21112b01edeb85
Parameswari Bala <parameswaribala@gmail.com>
2024-09-09 10:45:24 +0530

    updated

--- advjavaws/src/main/java/com/siemens/dao/ProductImpl.java
+++ advjavaws/src/main/java/com/siemens/dao/ProductImpl.java
@@ -2,16 +2,23 @@ package com.siemens.dao;
 
 import com.siemens.models.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductImpl implements ProductDAO {
+    private List<Product> productList;
+
+    public ProductImpl(){
+        productList=new ArrayList<>();
+    }
     @Override
     public Product addProduct(Product product) {
-        return null;
+        this.productList.add(product);
+        return product;
     }
 
     @Override
     public List<Product> getAllProducts() {
-        return null;
+        return productList;
     }
 }
