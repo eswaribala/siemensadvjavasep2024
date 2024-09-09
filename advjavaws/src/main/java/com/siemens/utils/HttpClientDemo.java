4a12002b0564d6866adfb3c7b3fd8b151bdd312c
Parameswari Bala <parameswaribala@gmail.com>
2024-09-09 16:44:55 +0530

    updated

--- advjavaws/src/main/java/com/siemens/utils/HttpClientDemo.java
+++ advjavaws/src/main/java/com/siemens/utils/HttpClientDemo.java
@@ -1,8 +1,10 @@
 package com.siemens.utils;
 
 import java.io.IOException;
+import java.net.Authenticator;
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
@@ -12,6 +14,7 @@ public class HttpClientDemo {
     private static final HttpClient httpClient=HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_1_1)
             .connectTimeout(Duration.ofMillis(5000))
+
             .build();
 
     public static void main(String[] args){
@@ -19,13 +22,19 @@ public class HttpClientDemo {
         HttpRequest httpRequest=HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create("https://restcountries.com/v2/all"))
+
                 .build();
 
         HttpResponse httpResponse=null;
 
         try {
             httpResponse=httpClient.send(httpRequest,HttpResponse.BodyHandlers.ofString());
-            System.out.println(httpResponse.body().toString());
+            //display only country names
+            HttpHeaders httpHeaders= httpResponse.headers();
+            httpHeaders.map().entrySet().stream().forEach(entry->System.out.println(entry.getKey()
+                    +","+entry.getValue()));
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
