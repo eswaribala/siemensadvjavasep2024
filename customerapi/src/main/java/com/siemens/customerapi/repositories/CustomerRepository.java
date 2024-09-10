95474eee746e2506b0bcd0ffd4eb57802c2c9db5
Parameswari Bala <parameswaribala@gmail.com>
2024-09-10 15:12:47 +0530

    updated

--- customerapi/src/main/java/com/siemens/customerapi/repositories/CustomerRepository.java
+++ customerapi/src/main/java/com/siemens/customerapi/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.siemens.customerapi.repositories;
 import com.siemens.customerapi.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByContactNo(long number);
 }
