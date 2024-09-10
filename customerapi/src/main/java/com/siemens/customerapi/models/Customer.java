d1da6a9ef3139bfa6a12299033c1ff9a2ae85505
Parameswari Bala <parameswaribala@gmail.com>
2024-09-10 13:16:06 +0530

    updated

---
+++ customerapi/src/main/java/com/siemens/customerapi/models/Customer.java
@@ -0,0 +1,30 @@
+package com.siemens.customerapi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@SuperBuilder
+@Table(name="Customer")
+@Inheritance(strategy = InheritanceType.JOINED)
+public sealed class Customer permits Individual,Corporate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="Account_No")
+    private long accountNo;
+    @Embedded
+    private FullName fullName;
+    @Column(name="Email", nullable = false,length = 150)
+    private String email;
+    @Column(name="Password", nullable = false,length = 10)
+    private String password;
+    @Column(name="Contact_No")
+    private long contactNo;
+
+}
