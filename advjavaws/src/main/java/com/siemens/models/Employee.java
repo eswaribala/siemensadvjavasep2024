1ac12a4c020e6c583793449a76f28d09c051316e
Parameswari Bala <parameswaribala@gmail.com>
2024-09-10 09:43:43 +0530

    updated

--- advjavaws/src/main/java/com/siemens/models/Employee.java
+++ advjavaws/src/main/java/com/siemens/models/Employee.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Employee {
+public sealed class Employee  permits  FullTimeEmployee,ContractEmployee{
     private int basicSalary;
     private double bonus;
 }
---
+++ advjavaws/src/main/java/com/siemens/models/FullTimeEmployee.java
@@ -0,0 +1,4 @@
+package com.siemens.models;
+
+public  non-sealed class FullTimeEmployee extends Employee{
+}
