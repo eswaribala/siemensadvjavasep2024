c65930c0b536bdb611016071d0c137898ca3dbd7
Parameswari Bala <parameswaribala@gmail.com>
2024-09-11 15:46:25 +0530

    updated

--- customerapiexternal/src/main/java/com/siemens/customerapi/CustomerapiApplication.java
+++ customerapiexternal/src/main/java/com/siemens/customerapi/CustomerapiApplication.java
@@ -2,8 +2,10 @@ package com.siemens.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerapiApplication {
 
     public static void main(String[] args) {
