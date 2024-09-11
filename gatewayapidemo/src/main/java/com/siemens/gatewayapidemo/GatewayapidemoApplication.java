a547fe68590fd8a0a856d3e06a42e7615ad4458a
Parameswari Bala <parameswaribala@gmail.com>
2024-09-11 16:46:17 +0530

    updated

---
+++ gatewayapidemo/src/main/java/com/siemens/gatewayapidemo/GatewayapidemoApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.gatewayapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayapidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayapidemoApplication.class, args);
+    }
+
+}
