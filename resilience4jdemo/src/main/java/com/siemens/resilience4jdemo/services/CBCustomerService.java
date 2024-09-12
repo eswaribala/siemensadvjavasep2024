df9d0459ca6e138a1f044a86131628aea6a460b4
Parameswari Bala <parameswaribala@gmail.com>
2024-09-12 11:52:53 +0530

    updated

--- resilience4jdemo/src/main/java/com/siemens/resilience4jdemo/services/CBCustomerService.java
+++ resilience4jdemo/src/main/java/com/siemens/resilience4jdemo/services/CBCustomerService.java
@@ -2,12 +2,17 @@ package com.siemens.resilience4jdemo.services;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Service
+@Slf4j
 public class CBCustomerService {
     @Autowired
     private RestClient restClient;
@@ -19,6 +24,8 @@ public class CBCustomerService {
     @CircuitBreaker(name = "gatewayCircuitBreaker", fallbackMethod = "fallback")
     @Retry(name = "gatewayRetry")
     public String getCustomerService(){
+
+        log.info("Entering Primary method @="+ LocalDateTime.now());
       return  restClient.get()
                 .uri(serviceUrl)
                 .retrieve()
@@ -26,7 +33,8 @@ public class CBCustomerService {
 
     }
 
-    public String fallback(){
+    public String fallback(Exception e){
+        log.info("Entering Fallback method @="+ LocalDateTime.now() + e.getMessage());
         return  restClient.get()
                 .uri(alternativeServiceUrl)
                 .retrieve()
