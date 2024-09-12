a8f7b964a2e5fe5f5dc70aeadc4cda24b8c8e458
Parameswari Bala <parameswaribala@gmail.com>
2024-09-12 16:58:56 +0530

    updated

--- customerapidocker/src/main/java/com/siemens/customerapi/controllers/CustomerController.java
+++ customerapidocker/src/main/java/com/siemens/customerapi/controllers/CustomerController.java
@@ -7,7 +7,6 @@ import com.siemens.customerapi.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -36,7 +35,6 @@ public class CustomerController {
     //retrieve all customers
     @GetMapping("/v1.0")
     @CrossOrigin("*")
-    @PreAuthorize("hasAuthority('SCOPE_Developer')")
     public List<Customer> fetchAllCustomers(){
         return this.customerService.getAllCustomers();
     }
